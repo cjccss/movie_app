f174fff3d49e151e0a98cff3f5777fd1ad24bc8e
cyh <cyh@mydata.re.kr>
2023-08-22 15:47:15 +0900

    feet: nav, 영화상세페이지 추가

---
+++ src/routes/About.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function About(){
+    return<span>About this page: I built it because </span>
+}
---
+++ src/routes/Detail.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+export default function Detail(){
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (location.state === null) {
+            navigate('/');
+        }
+    });
+
+    if (location.state) {
+        const { title } = location.state;
+        return <span>{title}</span>;
+    }
+    return null;
+}
